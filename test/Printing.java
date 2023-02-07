7e4b278d97e3b576103c70563c060ea7f92373a2
Allan Zhou <allanlinzhou@gmail.com>
2023-02-07 16:14:51 +0000

    Initial Commit

---
+++ test/Printing.java
@@ -0,0 +1,16 @@
+package test; 
+
+public class Printing {
+  public static void main(String [] args) {
+    System.out.println("Hello World!");
+    System.out.print("I am having pasta ");
+    System.out.println("for lunch today.");
+
+    // println adds a new line after, print does not.
+
+    System.out.println("\nMy name is Allan.");
+    System.out.println("Name\tPhone");
+    System.out.println("John\t123456");
+    System.out.println("Ann\t\t123456");
+  }
+}
