a08f0af61752cd47b82d71ffc525d872fceff7d0
Allan Zhou <allanlinzhou@gmail.com>
2023-05-04 23:30:26 -0400

    inheritance notes

---
+++ test2_notes/Hierarchy.java
@@ -0,0 +1,36 @@
+package test2_notes;
+
+public class Hierarchy {
+    /*
+     * Hierarchies 
+     * 
+     * Upward and downward relationship between entities
+     * 
+     * Ancestor - class above (superclass)
+     * Descendant - class below (subclass)
+     * 
+     * Keyword - extends 
+     * 
+     * Variables and methods can be inherited by descendants (subclass) from an ancestor (superclass)
+     * 
+     * public - variable or method can be accessed by any class 
+     * private - variable or method can be accessed only within the class 
+     * protected - variable or method can be accessed within the class + descendants of class
+     */
+
+    /*
+     * Extends 
+     * 
+     * Every class is a descendant of Object 
+     * 
+     * extends - immediate descendant of class it extends 
+     * 
+     * this - refers to current class 
+     * 
+     * super - refers to superclass, used to invoke its methods (e.g. constructor)
+     * 
+     * must be first statement in constructor of class 
+     * 
+     * class's method takes priority over superclass (method overriding)
+     */
+}
